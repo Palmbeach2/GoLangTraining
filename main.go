460b90040e4d7b5391d1d1e05d8f04a09c4b8424
Palmbeach2 <ralf-weinert@freenet.de>
2018-12-11 19:44:34 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	z := 2.01
+	x := 5
+	x = 7
+	c := 3i + 4
+
+	fmt.Println(x)
+	fmt.Println(z)
+	fmt.Println(c)
+	fmt.Println(name)
+	fmt.Println("Ready to go!")
+}
