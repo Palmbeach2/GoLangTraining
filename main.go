20fe1fbba7c6da40216c26a28620fe53ae57ec56
Palmbeach2 <ralf-weinert@freenet.de>
2018-12-11 19:56:17 +0100

    Funtions

--- main.go
+++ main.go
@@ -2,18 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
-	var name string
-	z := 2.01
-	x := 5
-	x = 7
-	c := 3i + 4
+func sayHello() {
+	fmt.Println("Hello")
+}
 
-	fmt.Println(x)
-	fmt.Println(z)
-	fmt.Println(c)
-	fmt.Println(name)
-	fmt.Println("Ready to go!")
-	fmt.Println(c)
+func main() {
+	sayHello()
 
 }
