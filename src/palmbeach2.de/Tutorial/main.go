19baf4d5b3b2ede718ff386175262373c9396b86
Palmbeach2 <ralf-weinert@freenet.de>
2018-12-16 23:48:44 +0100

    webserver ausgelagert

--- src/palmbeach2.de/Tutorial/main.go
+++ src/palmbeach2.de/Tutorial/main.go
@@ -9,9 +9,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello From Server")
 }
 
-func main() {
+func webserver() {
+
 	fmt.Println("Webserver")
 	http.HandleFunc("/hello", hello)
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+
 }
